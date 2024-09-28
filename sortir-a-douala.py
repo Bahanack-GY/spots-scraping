1f0def6def92c5396385595a831f1c41d0990c60
LeHavker <georgesyvan12@gmail.com>
2024-09-28 22:53:02 +0100

    Commit

--- sortir-a-douala.py
+++ sortir-a-douala.py
@@ -6,7 +6,7 @@ import requests
 session = HTMLSession()
 
 # Step 2: Fetch the page
-url = 'www.sortir-a-douala.com'
+url = 'yaounde-hangout.com'
 response = session.get(url)
 
 # Step 3: Render JavaScript (if necessary)
