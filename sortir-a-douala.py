22bc27ac2d37c7f98f22ef2d4ff6f20ce7e0187d
LeHavker <georgesyvan12@gmail.com>
2024-10-11 20:08:31 +0100

    Push

--- sortir-a-douala.py
+++ sortir-a-douala.py
@@ -6,7 +6,7 @@ import requests
 session = HTMLSession()
 
 # Step 2: Fetch the page
-url = 'spotcom.fr'
+url = 'spotcameroon.fr'
 response = session.get(url)
 
 # Step 3: Render JavaScript (if necessary)
