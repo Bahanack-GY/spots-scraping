860e229fa5468e1553637ad1e0a8bf37e07ec9bf
LeHavker <georgesyvan12@gmail.com>
2024-10-16 00:34:39 +0100

    Hekk

--- sortir-a-douala.py
+++ sortir-a-douala.py
@@ -6,7 +6,7 @@ import requests
 session = HTMLSession()
 
 # Step 2: Fetch the page
-url = 'spotwfsfn.fr'
+url = 'spotwn.fr'
 response = session.get(url)
 
 # Step 3: Render JavaScript (if necessary)
